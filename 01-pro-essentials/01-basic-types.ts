57af94e1fd2de2acc25525328580378754a7d30d
Josh Date <joshdate@Joshs-MacBook-Air.local>
2025-10-26 02:03:23 -0400

    completed basic-types

--- 01-pro-essentials/01-basic-types.ts
+++ 01-pro-essentials/01-basic-types.ts
@@ -12,13 +12,31 @@ let chaos: any = 42;
 // TODO: write a function that safely logs the type of an unknown value
 // without using "any"
 
+function logType(value: unknown): void {
+  console.log(value)
+}
+
+
 // Union drill
 // TODO: Create a function that accepts string | number and returns a consistent string
 
+function unionFunction(value: string | number){
+  return typeof value === 'string' ? value : String(value) 
+}
+
 // Challenge: refactor this function to remove 'any'
-function double(value: any) {
+function double(value: number) {
   return value * 2;
 }
 
 // Optional: Try making a literal type for roles ('user' | 'admin' | 'guest')
 // and create a function that enforces it
+
+type Roles = 'user' | 'admin' | 'guest'
+
+
+function literalType(role: Roles): Roles{
+  return role;
+}
+
+literalType('admin')
